6c985c6253767e484c598adbee6009fff115afb5
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-03 09:41:45 +0530

    Add light and dark theme support

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,24 @@ void main() {
   runApp(const ArrowGameApp());
 }
 
-class ArrowGameApp extends StatelessWidget {
+class ArrowGameApp extends StatefulWidget {
   const ArrowGameApp({super.key});
+  @override
+  State<ArrowGameApp> createState() => _ArrowGameAppState();
+}
+
+class _ArrowGameAppState extends State<ArrowGameApp> {
+  ThemeMode _mode = ThemeMode.light;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Arrow Game',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const LevelSelectionScreen(),
+      themeMode: _mode,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue), useMaterial3: true, brightness: Brightness.light),
+      darkTheme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo, brightness: Brightness.dark), useMaterial3: true, brightness: Brightness.dark),
+      home: LevelSelectionScreen(onThemeChanged: (mode) => setState(() => _mode = mode)),
     );
   }
 }

ce7207587427374d9b363eadc50fcccbb0f0c6e9
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-03 09:29:30 +0530

    Add arrow cell model

---
+++ lib/models/arrow_cell.dart
@@ -0,0 +1,14 @@
+import 'arrow_direction.dart';
+
+class ArrowCell {
+  ArrowCell({required this.row, required this.col, Set<ArrowDirection>? arrows})
+      : arrows = arrows ?? <ArrowDirection>{};
+
+  final int row;
+  final int col;
+  final Set<ArrowDirection> arrows;
+
+  bool has(ArrowDirection direction) => arrows.contains(direction);
+
+  bool get isDeadEnd => arrows.length <= 1;
+}
